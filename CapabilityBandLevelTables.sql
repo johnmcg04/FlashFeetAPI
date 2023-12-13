092554360d8a6c923c9a29e3ad5bd5ca1c17813b
Breanna-loughrey <145446184+Breanna-loughrey@users.noreply.github.com>
2023-12-13 15:48:45 +0000

    Update and rename Database to CapabilityBandLevelTables.sql

--- CapabilityBandLevelTables.sql
+++ CapabilityBandLevelTables.sql
@@ -1,20 +1,28 @@
-CREATE TABLE Capability 
-( 
+USE FlashFeet_JohnMc;
+
+DELIMITER $$
+DROP PROCEDURE IF EXISTS CreateCapabilityBandLevel $$
+CREATE PROCEDURE CreateCapabilityBandLevel()
+BEGIN
+
+CREATE TABLE IF NOT EXISTS Capability ( 
 capability			varchar(30)		NOT NULL, 
 CONSTRAINT pk_jobfamily PRIMARY KEY (capability) 
 ); 
-
 ALTER TABLE JobRole 
 ADD CONSTRAINT fk_jobrole_capability FOREIGN KEY (capability) REFERENCES Capability(capability);
 
-CREATE TABLE BandLevel( 
+CREATE TABLE IF NOT EXISTS BandLevel( 
 bandLevel			varchar(70)		NOT NULL, 
 CONSTRAINT pk_bandLevel PRIMARY KEY (bandLevel) 
 ); 
-
 ALTER TABLE JobRole
 ADD CONSTRAINT fk_jobrole_blevel FOREIGN KEY (bandLevel) REFERENCES BandLevel(bandLevel);
 
 ALTER TABLE JobRole
 ADD COLUMN jobFamily	varchar(70)		NOT NULL
 AFTER bandLevel;
+
+END $$
+DELIMITER ;
+CALL CreateCapabilityBandLevel();

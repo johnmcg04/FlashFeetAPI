8f35583e781bd999575e9c2d34eba1af0cc272d8
CDawson8 <cory.dawson@kainos.com>
2023-12-14 16:16:28 +0000

    Removal of controller, Dao, service and class that are no longer necessary

--- FlashFeetAPI/src/main/java/org/example/client/FailedToGetCapability.java
+++
@@ -1,8 +0,0 @@
-package org.example.client;
-
-public class FailedToGetCapability extends Throwable{
-    @Override
-    public String getMessage(){
-        return "Failed to get job capability";
-    }
-}

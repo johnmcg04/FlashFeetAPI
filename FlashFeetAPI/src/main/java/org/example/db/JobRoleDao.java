1c119771142c7a35a924a057680ed80b1e86f2cd
Breanna-loughrey <145446184+Breanna-loughrey@users.noreply.github.com>
2023-12-21 09:46:35 +0000

    Updated JobRoleDao.java

--- FlashFeetAPI/src/main/java/org/example/db/JobRoleDao.java
+++ FlashFeetAPI/src/main/java/org/example/db/JobRoleDao.java
@@ -10,8 +10,6 @@ public class JobRoleDao {
     private DatabaseConnector databaseConnector = new DatabaseConnector();
     public List<JobRole> getAllJobRoles(Connection connection) throws SQLException {
 
-        // Connection c = databaseConnector.getConnection();
-
         Statement st = connection.createStatement();
 
         ResultSet rs = st.executeQuery("SELECT jobRole FROM JobRole");

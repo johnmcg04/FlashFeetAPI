c63c311286906b478e4c8611f559bb01d3bef8e7
johnmcg04 <john.mcglinchey@kainos.com>
2024-01-09 12:12:00 +0000

    Updated DAO

--- FlashFeetAPI/src/main/java/org/example/db/SignUpDao.java
+++ FlashFeetAPI/src/main/java/org/example/db/SignUpDao.java
@@ -1,6 +1,7 @@
 package org.example.db;
 
 import org.apache.commons.lang3.time.DateUtils;
+import org.example.api.SignUpService;
 import org.example.cli.Login;
 import org.example.cli.SignUp;
 
@@ -15,21 +16,26 @@ public class SignUpDao {
 
     public static int signUpUser(SignUp signUp, Connection c) {
         try {
-            String qryInsertSignUpDetails = "INSERT INTO `User` (Username, Password, RoleID) VALUES(?,?,2);";
+            SignUp saltedSignUpDetails = SignUpService.SaltUsernameAndPassword(signUp);
+            SignUp hashedSignUpDetails = SignUpService.HashUsernameAndPassword(saltedSignUpDetails);
+
+            String qryInsertSignUpDetails = "INSERT INTO `User` (Username, Password, Salt, RoleID) VALUES(?,?,?,2);";
             PreparedStatement preparedStatement = c.prepareStatement(qryInsertSignUpDetails);
-            preparedStatement.setString(1, signUp.getUsername());
-            preparedStatement.setString(2, signUp.getPassword());
+            preparedStatement.setString(1, hashedSignUpDetails.getUsername());
+            preparedStatement.setString(2, hashedSignUpDetails.getPassword());
+            preparedStatement.setString(3, hashedSignUpDetails.getSalt());
             int rowsInserted = preparedStatement.executeUpdate();
             if (rowsInserted > 0) {
                 return 1;
             } else {
                 return -1;
             }
-        } catch (SQLException e) {
+        } catch (Exception e) {
             System.err.println(e.getMessage());
             return -1;
         }
     }
+
 
 }
 

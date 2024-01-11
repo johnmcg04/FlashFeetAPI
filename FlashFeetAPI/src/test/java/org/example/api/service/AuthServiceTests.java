4bb7a737d79078191f70dd2e643d6572351e9803
johnmcg04 <john.mcglinchey@kainos.com>
2024-01-11 14:58:17 +0000

    Removal of commented code

--- FlashFeetAPI/src/test/java/org/example/api/service/AuthServiceTests.java
+++ FlashFeetAPI/src/test/java/org/example/api/service/AuthServiceTests.java
@@ -32,27 +32,6 @@ public class AuthServiceTests {
     public AuthServiceTests() throws DatabaseConnectionException, SQLException {
     }
 
-//    @Test
-//    @DisplayName("Testing valid login details")
-//    public void login_ShouldReturnTrue_WhenValidLoginReturnsTrue() throws SQLException, FailedTogenerateTokenException, FailedToLoginException, DatabaseConnectionException {
-//        Login validLogin = new Login("User123!","User123!");
-//        when(databaseConnector.getConnection()).thenReturn(c);
-//        when(authDao.validLogin(validLogin, c)).thenReturn(Boolean.TRUE);
-//        if(authService.login(validLogin) instanceof String){ //returns a string i.e. returns the token
-//            assertTrue(true);
-//        }
-//        else fail();
-//    }
-//
-//    @Test
-//    @DisplayName("Testing invalid login details")
-//    public void login_ShouldReturnException_WhenValidLoginReturnsFalse() throws SQLException, DatabaseConnectionException {
-//        Login inValidLogin = new Login("invalidLogin6484846!!!!547r76rybfhtfjjvgv","pwordhvgvjvggvvggvggvv-");
-//        when(databaseConnector.getConnection()).thenReturn(c);
-//        when(authDao.validLogin(inValidLogin, c)).thenReturn(Boolean.FALSE);
-//        assertThrows(FailedToLoginException.class, () -> authService.login(inValidLogin));
-//    }
-
 
 
 }

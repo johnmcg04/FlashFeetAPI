42243c2bbb055db49a927a061ad7228d987eda8b
Breanna-loughrey <breanna.loughrey@kainos.com>
2024-01-11 09:37:32 +0000

    changing validator to 70 and removing comment

--- FlashFeetAPI/src/main/java/org/example/validator/JobRoleValidator.java
+++ FlashFeetAPI/src/main/java/org/example/validator/JobRoleValidator.java
@@ -5,10 +5,9 @@ import org.example.cli.JobEntry;
 public class JobRoleValidator {
 
     public boolean isValidJobRole(JobEntry jobRole){
-        if(jobRole.getJobRole().length() > 50){
+        if(jobRole.getJobRole().length() > 70){
             return false;
         }
-        // more validation needed as fields are added further down the tickets
         return true;
     }
 

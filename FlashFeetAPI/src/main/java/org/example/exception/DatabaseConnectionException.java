1dccfab10bde9472b7368f13fe7a328a6cc55f20
Breanna-loughrey <breanna.loughrey@kainos.com>
2024-01-11 15:31:20 +0000

    fixing conflict

--- FlashFeetAPI/src/main/java/org/example/exception/DatabaseConnectionException.java
+++ FlashFeetAPI/src/main/java/org/example/exception/DatabaseConnectionException.java
@@ -1,10 +1,7 @@
 package org.example.exception;
 
 public class DatabaseConnectionException extends Throwable {
-<<<<<<< HEAD
-=======
-  
->>>>>>> Pre-Production
+
     public DatabaseConnectionException(Exception e) {
         super(e);
     }

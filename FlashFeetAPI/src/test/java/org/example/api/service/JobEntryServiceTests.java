73d5b1c6a1e88e40c38661c5146403353d48e4d2
Breanna-loughrey <breanna.loughrey@kainos.com>
2024-01-11 10:15:09 +0000

    created test

--- FlashFeetAPI/src/test/java/org/example/api/service/JobEntryServiceTests.java
+++ FlashFeetAPI/src/test/java/org/example/api/service/JobEntryServiceTests.java
@@ -2,7 +2,10 @@ package org.example.api.service;
 
 import org.example.api.JobEntryService;
 import org.example.cli.JobEntry;
+import org.example.cli.JobEntryRequest;
+import org.example.client.FailedToCreateJobEntryException;
 import org.example.client.FailedToGetJobEntriesException;
+import org.example.client.InvalidJobEntryException;
 import org.example.client.JobEntryDoesNotExistException;
 import org.example.db.DatabaseConnector;
 import org.example.db.JobEntryDao;
@@ -18,6 +21,7 @@ import java.util.Collections;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.mock;
 
 public class JobEntryServiceTests {
@@ -25,7 +29,7 @@ public class JobEntryServiceTests {
     JobEntryDao jobRoleDao = mock(JobEntryDao.class);
     JobEntryValidator jobEntryValidator = mock(JobEntryValidator.class);
 
-    JobEntryService jobRoleService = new JobEntryService( jobRoleDao, jobEntryValidator);
+    JobEntryService jobRoleService = new JobEntryService(jobRoleDao, jobEntryValidator);
 
     Connection c;
 
@@ -43,5 +47,19 @@ public class JobEntryServiceTests {
         assertEquals(listOfJobRoles, jobRoleService.getAllJobEntries());
     }
 
+    @Test
+    void createJobRole_shouldReturnTrue_whenDaoReturnsTrue() throws SQLException, FailedToCreateJobEntryException, InvalidJobEntryException {
 
+        JobEntryRequest jobEntry = new JobEntryRequest(
+                "Job Role",
+                "Job Spec Link",
+                "Capability",
+                "Band Level",
+                "Job Family",
+                "Responsibilities",
+                "Job Spec Summary");
+
+        Mockito.when(jobRoleDao.createJobEntry(jobEntry)).thenReturn(true);
+        assertEquals(true, jobRoleService.createJobEntry(jobEntry));
+    }
 }

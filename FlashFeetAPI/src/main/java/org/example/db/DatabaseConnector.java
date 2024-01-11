930dc6a8cc303a3a55b61ecd1820a24087ac3ecb
johnmcg04 <john.mcglinchey@kainos.com>
2024-01-11 13:02:27 +0000

    updated connection

--- FlashFeetAPI/src/main/java/org/example/db/DatabaseConnector.java
+++ FlashFeetAPI/src/main/java/org/example/db/DatabaseConnector.java
@@ -5,40 +5,39 @@ import org.example.exception.DatabaseConnectionException;
 import java.io.FileInputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.SQLException;
 import java.util.Properties;
 
 public class DatabaseConnector {
     private static Connection conn;
 
-    public Connection getConnection() throws DatabaseConnectionException, SQLException {
-        String user;
-        String password;
-        String host;
-        String database;
+    public Connection getConnection() {
+        String user, password, host, name;
 
-        if (conn != null && !conn.isClosed()) {
+        if (conn != null) {
             return conn;
         }
 
-        try {
-            user            = System.getenv("DB_USERNAME");
-            password        = System.getenv("DB_PASSWORD");
-            host            = System.getenv("DB_HOST");
-            database        = System.getenv("DB_NAME");
+        try (FileInputStream propsStream = new FileInputStream("db.properties")) {
+            Properties props = new Properties();
+            props.load(propsStream);
+
+            user = props.getProperty("user");
+            password = props.getProperty("password");
+            host = props.getProperty("host");
+            name = props.getProperty("name");
 
             if (user == null || password == null || host == null)
-                throw new IllegalArgumentException(
-                        "Environment variables not set.");
+                throw new IllegalArgumentException("Properties file must exist " +
+                        "and must contain user, password, name and host properties.");
 
-            conn = DriverManager.getConnection("jdbc:mysql://"
-                    + host + "/" + database + "?allowPublicKeyRetrieval=true&useSSL=false", user, password);
-
+            conn = DriverManager.getConnection("jdbc:mysql://" + host + "/" + name + "?useSSL=false", user, password);
             return conn;
+
         } catch (Exception e) {
-            throw new DatabaseConnectionException(e);
+            System.err.println(e.getMessage());
+        } finally {
+            System.out.println("I will always run!");
         }
+        return null;
     }
 }
-
-
